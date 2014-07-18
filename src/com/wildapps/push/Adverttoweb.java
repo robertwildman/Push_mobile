725718691f514fd339c4d6871544e150a57dc644
Robert Wildman <Robertwildman4@gmail.com>
2014-07-18 21:37:04 +0100

    Adding Adverts
    Add adverts to the app when you click on the push notification it will
    show up before taking you to the web page.

--- src/com/wildapps/push/Adverttoweb.java
+++ src/com/wildapps/push/Adverttoweb.java
@@ -1,28 +1,46 @@
 package com.wildapps.push;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
 public class Adverttoweb extends Activity{
-
+	 private InterstitialAd interstitial;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.advertview);
 		//Will have the adverts displayed here.
+		  // Create the interstitial.
+	    interstitial = new InterstitialAd(this);
+	    interstitial.setAdUnitId("ca-app-pub-2049126681125303/9962405275");
+
+	    // Create ad request.
+	    AdRequest adRequest = new AdRequest.Builder().build();
+
+	    // Begin loading your interstitial.
+	    interstitial.loadAd(adRequest);
 		
 		
 		
-		Intent i = new Intent(Intent.ACTION_VIEW);
-		Bundle extras = getIntent().getExtras();
-		String url = extras.getString("url");
-		i.setData(Uri.parse(url));
-		if(url.isEmpty() == false)
-		{	
-		startActivity(i);
-		}
 		
 	}
+	public void displayInterstitial() {
+	    if (interstitial.isLoaded()) {
+	      interstitial.show();
+	      Intent i = new Intent(Intent.ACTION_VIEW);
+			Bundle extras = getIntent().getExtras();
+			String url = extras.getString("url");
+			i.setData(Uri.parse(url));
+			if(url.isEmpty() == false)
+			{	
+			startActivity(i);
+			}
+	    }
+	    
+	  }
 }
